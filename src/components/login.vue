d0ca0235ed0ed9d447b829317cf016082b7165e5
Nkechi Julia <nkechionyimadu@gmail.com>
2022-08-26 22:39:59 +0100

    worked on deploy error

--- src/components/login.vue
+++ src/components/login.vue
@@ -96,7 +96,7 @@
         alertTitle = ""
         alertType = ""
         alertShow = false
-        mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+        mailformat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
         //To check a password between 6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter
         regPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/
         error = null
