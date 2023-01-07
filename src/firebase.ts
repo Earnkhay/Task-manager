1b1d48f79159b3b73a3895587317fcb47cdf4303
Julia <earnkay2@gmail.com>
2023-01-08 00:06:24 +0100

    add firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDISfT_jx0hkD-SDgy5Z8tlPVT79dNEdXg",
+  authDomain: "vue-http-learning-b7e81.firebaseapp.com",
+  databaseURL: "https://vue-http-learning-b7e81-default-rtdb.firebaseio.com",
+  projectId: "vue-http-learning-b7e81",
+  storageBucket: "vue-http-learning-b7e81.appspot.com",
+  messagingSenderId: "512051751261",
+  appId: "1:512051751261:web:5ae2d5b116bbeed2efe0b4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db }
